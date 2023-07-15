f4589c0bc7e9c4f6706adb090806b81f9e81229b
AleMenzi <hectoralejandro_95@hotmail.com>
2023-07-15 12:37:08 -0300

    Creación de formulario

--- my-react-app/src/Componentes/Formulario/Formulario.jsx
+++ my-react-app/src/Componentes/Formulario/Formulario.jsx
@@ -1,7 +1,6 @@
 import { useContext, useState } from "react";
 import { collection, addDoc } from "firebase/firestore";
 import { db } from "../../services/config";
-
 import React from 'react'
 import { CarritoContext } from "../../Context/CarritoContext";
 
@@ -15,36 +14,96 @@ const Formulario = () => {
         const [ordenId, setOrdenId] = useState("");
 
         const {carrito, vaciarCarrito, total, cantTotal} = useContext(CarritoContext)
+
+        const manejadorFormulario = (event) => {
+          event.preventDefault();
+  
+
+          if (!nombre || !apellido || !telefono || !email || !emailConfirmacion) {
+              setError("Complete todos los datos");
+              return;
+          }
+   
+          if (email !== emailConfirmacion) {
+              setError("Los campos del email no coinciden");
+              return;
+          }
+  
+          const orden = {
+              items: carrito.map(producto => ({
+                  id: producto.item.id,
+                  nombre: producto.item.nombre,
+                  cantidad: producto.cantidad
+              })),
+              total: total,
+              fecha: new Date(),
+              nombre,
+              apellido,
+              telefono,
+              email
+          };
+  
+          addDoc(collection(db, "ordenes"), orden)
+              .then(docRef => {
+                  setOrdenId(docRef.id);
+                  vaciarCarrito();
+              })
+              .catch(error => {
+                  console.log("Error al crear la orden", error);
+                  setError("Se produjo un error al crear la orden");
+              })
+      }
   return (
     <div>
       <h3> Formulario para compradores</h3>
       
       <form onSubmit={manejadorFormulario}>
-              
-
+        {
+          carrito.map(producto =>(
+          <div key={producto.item.id}>
+            <p> {producto.item.nombre} x {producto.cantidad} </p>
+            <p> {producto.item.precio} </p>
+            <hr />
+          </div>
+        ))
+        }   
+        <hr />
+            <div className="form-group">
               <label htmlFor="">Nombre </label>
               <input type="text" value={nombre} onChange={(e) => setNombre(e.target.value)}/>
+            </div>
 
+            <div className="form-group">
               <label htmlFor="">Apellido </label>
               <input type="text" value={apellido} onChange={(e) => setApellido(e.target.value)}/>
+            </div>
 
+            <div className="form-group">
               <label htmlFor="">Telefono </label>
               <input type="text" value={Telefono} onChange={(e) => setTelefono(e.target.value)}/>
+            </div>
 
+            <div className="form-group">
               <label htmlFor="">Email </label>
               <input type="text" value={email} onChange={(e) => setEmail(e.target.value)}/>
+            </div>
 
+            <div className="form-group">
               <label htmlFor="">Email Confirmacion </label>
               <input type="text" value={emailConfirmacion} onChange={(e) => setEmailConfirmacion(e.target.value)}/>
-
-
-              <button type=""> Enviar </button>
+            </div>
               
             {
-              error && <p style={{color:"red"}}></p>
+              error && <p style={{color:"red"}}> {error}</p>
             }
-          <button type=""> Reset </button>
+          <button type=""> Finalizar Compra </button>
       </form>
+      {
+        ordenId && (
+          <> Gracias por tu compra. Se generó su orden y es la número {ordenId}
+          </>
+        )
+      }
     </div>
   )
 }
